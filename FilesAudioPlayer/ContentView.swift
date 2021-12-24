44802a3746dbd84ea0a3aa7e3986f7220de2575e
Aidan Lee <aidanwlee@gmail.com>
2021-12-24 13:48:39 -0500

    Initial Commit

---
+++ FilesAudioPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FilesAudioPlayer
+//
+//  Created by Aidan Lee on 12/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FilesAudioPlayer/FilesAudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilesAudioPlayerApp.swift
+//  FilesAudioPlayer
+//
+//  Created by Aidan Lee on 12/24/21.
+//
+
+import SwiftUI
+
+@main
+struct FilesAudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
